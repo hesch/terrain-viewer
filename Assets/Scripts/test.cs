6985b0e4adcf67c4240478bdeb54ad7ff76d4e14
Henri Schmidt <henri@reschmi.de>
2019-02-01 16:46:59 +0100

    add inital unity stuff

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      Debug.Log("test");
+      gameObject.AddComponent<MeshFilter>();
+        gameObject.AddComponent<MeshRenderer>();
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+
+        mesh.Clear();
+
+        // make changes to the Mesh by creating arrays which contain the new values
+        mesh.vertices = new Vector3[] {new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(1, 1, 0)};
+        mesh.uv = new Vector2[] {new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1)};
+        mesh.triangles =  new int[] {0, 1, 2};
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
