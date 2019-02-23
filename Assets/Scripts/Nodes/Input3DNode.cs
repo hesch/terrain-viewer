50570d201a10b35e8c3cbff8b25f8be0dd348c0d
Henri Schmidt <henri@reschmi.de>
2019-02-23 22:18:20 +0100

    add some custom nodes

---
+++ Assets/Scripts/Nodes/Input3DNode.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using NodeEditorFramework;
+using NodeEditorFramework.Utilities;
+
+[Node (false, "Scripts/Node/Input3DNode")]
+public class Input3DNode : Node 
+{
+	public const string ID = "Input3D";
+	public override string GetID { get { return ID; } }
+
+	public override string Title { get { return "Input Node"; } }
+	public override Vector2 DefaultSize { get { return new Vector2 (150, 100); } }
+
+	public Vector3Int value = Vector3Int.zero;
+
+	[ValueConnectionKnob("X", Direction.Out, "Int")]
+		public ValueConnectionKnob xConnection;
+	[ValueConnectionKnob("Y", Direction.Out, "Int")]
+		public ValueConnectionKnob yConnection;
+	[ValueConnectionKnob("Z", Direction.Out, "Int")]
+		public ValueConnectionKnob zConnection;
+
+	public override void NodeGUI () 
+	{
+		name = RTEditorGUI.TextField (name);
+
+		foreach (ValueConnectionKnob knob in connectionKnobs) 
+			knob.DisplayLayout ();
+	}
+
+	public override bool Calculate() {
+		xConnection.SetValue<int>(value.x);
+		yConnection.SetValue<int>(value.y);
+		zConnection.SetValue<int>(value.z);
+		return true;
+	}
+}
---
+++ Assets/Scripts/Nodes/Noise3DNode.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using NodeEditorFramework;
+using NodeEditorFramework.Utilities;
+
+[Node (false, "Scripts/Node/Noise3DNode")]
+public class Noise3DNode : Node 
+{
+	public const string ID = "Noise3D";
+	public override string GetID { get { return ID; } }
+
+	public override string Title { get { return "Noise"; } }
+	public override Vector2 DefaultSize { get { return new Vector2 (150, 100); } }
+
+	[ValueConnectionKnob("X", Direction.In, "Int")]
+		public ValueConnectionKnob xConnection;
+	[ValueConnectionKnob("Y", Direction.In, "Int")]
+		public ValueConnectionKnob yConnection;
+	[ValueConnectionKnob("Z", Direction.In, "Int")]
+		public ValueConnectionKnob zConnection;
+
+	public override void NodeGUI () 
+	{
+		name = RTEditorGUI.TextField (name);
+
+		foreach (ValueConnectionKnob knob in connectionKnobs) 
+			knob.DisplayLayout ();
+	}
+
+	public override bool Calculate() {
+		xConnection.SetValue<int>(value.x);
+		yConnection.SetValue<int>(value.y);
+		zConnection.SetValue<int>(value.z);
+		return true;
+	}
+}
+
