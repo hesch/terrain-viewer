ab3895df219d2145acdbd0471f264e876dc95e11
Henri Schmidt <henri@reschmi.de>
2019-02-25 15:38:46 +0100

    fix compile errors

--- Assets/Scripts/Nodes/Noise3DNode.cs
+++ Assets/Scripts/Nodes/Noise3DNode.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using NodeEditorFramework;
 using NodeEditorFramework.Utilities;
+using ProceduralNoiseProject;
 
 [Node (false, "Scripts/Node/Noise3DNode")]
 public class Noise3DNode : Node 
@@ -31,11 +32,11 @@ public class Noise3DNode : Node
 	}
 
 	public override bool Calculate() {
-	  int x = xConnection.getValue<Int>();
-	  int y = yConnection.getValue<Int>();
-	  int z = zConnection.getValue<Int>();
-	  float noiseValue = perlin.sample3D(x, y, z);
-	  outputConnection.setValue<Float>(noiseValue);
+	  int x = xConnection.GetValue<int>();
+	  int y = yConnection.GetValue<int>();
+	  int z = zConnection.GetValue<int>();
+	  float noiseValue = perlin.Sample3D(x, y, z);
+	  outputConnection.SetValue<float>(noiseValue);
 	  return true;
 	}
 }
