cc9cd4203d40bfae819a14f0e6fdd35dc35bde40
Henri Schmidt <henri@reschmi.de>
2019-03-29 16:03:37 +0100

    WIP: add custom marching cubes implementation

---
+++ Assets/Scripts/MarchingCubesTest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarchingCubesTester : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
