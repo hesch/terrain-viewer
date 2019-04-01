d103dbd29494c71b30cfec14781acb7978c0dedf
Henri Schmidt <henri@reschmi.de>
2019-04-01 13:18:01 +0200

    CameraControl is now more better

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
   float dragSpeed = 2.0f;
-  float rotationSpeed = 2.0f;
+  float rotationSpeed = 0.5f;
+  float zoomSpeed = 2.0f;
   private Vector3 origin;
   private Vector3 oldLocalPosition;
   private Quaternion oldLocalRotation;
-  private Camera controlledCamera;
+  public Camera controlledCamera;
+  private RectTransform parentRect;
     // Start is called before the first frame update
     void Start()
     {
-      controlledCamera = GetComponent<Camera>();
+      parentRect = GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+      if (!RectTransformUtility.RectangleContainsScreenPoint(parentRect, Input.mousePosition))
+	return;
+
       handleZoom();
       handlePanning();
       handleRotation();
@@ -36,31 +41,31 @@ public class CameraController : MonoBehaviour
       Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - origin);
       Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
 
-      transform.Translate(move, Space.World);  
+      controlledCamera.transform.Translate(move, Space.World);  
     }
 
     private void handleRotation() {
       if (Input.GetMouseButtonDown(0))
       {
 	origin = Input.mousePosition;
-	oldLocalPosition = transform.localPosition;
-	oldLocalRotation = transform.localRotation;
+	oldLocalPosition = controlledCamera.transform.localPosition;
+	oldLocalRotation = controlledCamera.transform.localRotation;
 	return;
       }
 
       if (!Input.GetMouseButton(0)) return;
 
-      transform.localPosition = oldLocalPosition;
-      transform.localRotation = oldLocalRotation;
-      Vector3 pivot = transform.position + transform.forward * 50;
-      transform.RotateAround(pivot, Vector3.Cross(Input.mousePosition - origin, pivot-transform.position), (Input.mousePosition - origin).magnitude);
+      controlledCamera.transform.localPosition = oldLocalPosition;
+      controlledCamera.transform.localRotation = oldLocalRotation;
+      Vector3 pivot = controlledCamera.transform.position + controlledCamera.transform.forward * 75;
+      controlledCamera.transform.RotateAround(pivot, Vector3.Cross(pivot-controlledCamera.transform.position, Input.mousePosition - origin), (Input.mousePosition - origin).magnitude*rotationSpeed);
     }
 
     private void handleZoom() {
       if (Input.GetAxis("Mouse ScrollWheel") < 0) {
-	controlledCamera.fieldOfView++;
+	controlledCamera.transform.Translate(0, 0, -zoomSpeed);
       } else if (Input.GetAxis("Mouse ScrollWheel") > 0) {
-	controlledCamera.fieldOfView--;
+	controlledCamera.transform.Translate(0, 0, zoomSpeed);
       }
     }
 
