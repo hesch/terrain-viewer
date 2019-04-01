2cc419f7e6ad996ca16c671f7aa8ccfe24b8b6b8
Henri Schmidt <henri@reschmi.de>
2019-04-01 12:11:05 +0200

    work on marching cubes compute shader

--- Assets/Scripts/MarchingCubesTest.cs
+++ Assets/Scripts/MarchingCubesTest.cs
@@ -2,10 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MarchingCubesTester : MonoBehaviour
+public class MarchingCubesTest : MonoBehaviour
 {
+  public ComputeShader shader;
+  private int kernelIndex;
     void Start()
     {
+      kernelIndex = shader.FindKernel("computeSizes");
+
+      ComputeBuffer buffer = new ComputeBuffer(8, sizeof(int));
+      buffer.SetData(new []{1, 1, 0, 0, 1, 0, 1, 1});
+
+      shader.SetBuffer(kernelIndex, "buffer", buffer);
+      shader.Dispatch(kernelIndex, 1, 1, 1);
+      int[] result = new int[8];
+      buffer.GetData(result);
+      Debug.Log(result);
     }
 
     // Update is called once per frame
