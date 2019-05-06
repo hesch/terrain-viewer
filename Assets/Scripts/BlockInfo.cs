5e106ae0b7032567bdb8d113e9034036b98f2386
Henri Schmidt <henri@reschmi.de>
2019-05-06 23:55:54 +0200

    add debug rendering of normals in BlockInfo

--- Assets/Scripts/BlockInfo.cs
+++ Assets/Scripts/BlockInfo.cs
@@ -1,5 +1,26 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class BlockInfo : MonoBehaviour {
   public IVoxelBlock Block { get; set; }
+
+
+  private Mesh mesh;
+  private bool first = true;
+  public void Start() {
+    mesh = GetComponent<MeshFilter>().mesh;
+  }
+
+  public void Update() {
+    Vector3[] norms = mesh.normals;
+    Vector3[] verts = mesh.vertices;
+
+    if (Block.Offset.x == 0 && Block.Offset.y == 0) {
+      for(int i = 0; i < norms.Length; i++) {
+	verts[i] = transform.TransformPoint(verts[i]);
+	norms[i] = transform.TransformDirection(norms[i]);
+	  Debug.DrawLine(verts[i], verts[i] + norms[i], Color.yellow, 0.0f, true);
+      }
+    }
+  }
 }
