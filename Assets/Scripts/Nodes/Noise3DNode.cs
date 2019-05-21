86f50e4d5cdbeb1d6c13c96fd31937ed2fd8b77c
Henri Schmidt <henri@reschmi.de>
2019-05-21 17:29:29 +0200

    WIP: use NoiseGUI in Noise3DNode

--- Assets/Scripts/Nodes/Noise3DNode.cs
+++ Assets/Scripts/Nodes/Noise3DNode.cs
@@ -12,9 +12,7 @@ public class Noise3DNode : VoxelNode<Voxel>
 	public override string Title { get { return "Noise3D"; } }
 	public override Vector2 DefaultSize { get { return new Vector2 (150, 100); } }
 
-	private NoiseType noiseType;
 	private INoise noiseFunction;
-	private FractalNoise fractal;
 
 	private int width = 0;
 	private int height = 0;
@@ -22,49 +20,10 @@ public class Noise3DNode : VoxelNode<Voxel>
 	private Vector2Int offset;
 
 	public Noise3DNode() {
-	  noiseFunction = new PerlinNoise(1337, 2.0f);
-	  fractal = new FractalNoise(noiseFunction, 3, 1.0f);
-	}
-
-	private float normalize(float val) {
-	  switch(noiseType) {
-	    case NoiseType.Perlin:
-	    case NoiseType.Simplex:
-	    case NoiseType.Value:
-	    case NoiseType.Voronoi:
-	      return 1-(val+1)/2;
-	    case NoiseType.Worley:
-	      return 1-val;
-	  }
-	  return val;
 	}
 
 	public override void NodeGUI() {
 	  base.NodeGUI();
-	  RTEditorGUI.EnumPopup (new GUIContent ("Noise", "The noise type to use"), noiseType, n => {
-	      if (noiseType != n) {
-	      noiseType = n;
-	      switch (noiseType) {
-	      case NoiseType.Perlin:
-		noiseFunction = new PerlinNoise(1337, 2.0f);
-	      break;
-	      case NoiseType.Simplex:
-		noiseFunction = new SimplexNoise(1337, 2.0f);
-	      break;
-	      case NoiseType.Value:
-		noiseFunction = new ValueNoise(1337, 2.0f);
-	      break;
-	      case NoiseType.Voronoi:
-		noiseFunction = new VoronoiNoise(1337, 2.0f);
-	      break;
-	      case NoiseType.Worley:
-		noiseFunction = new WorleyNoise(1337, 2.0f, 1.0f);
-	      break;
-	      }
-	      fractal = new FractalNoise(noiseFunction, 3, 1.0f);
-	      NodeEditor.curNodeCanvas.OnNodeChange(this);
-	    }
-	  });
 	}
 
 	protected override void CalculationSetup(VoxelBlock<Voxel> block) {
