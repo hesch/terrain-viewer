0426b5c9f89a93e6b929380f71ba2aeee7553b19
Henri Schmidt <henri@reschmi.de>
2019-10-29 15:20:14 +0100

    add POC files

---
+++ Assets/Scripts/Shader/POC.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class POC : MonoBehaviour
+{
+    public ComputeShader POCShader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public ComputeBuffer invokeShader(string name, ComputeBuffer input) {
+	int kernelIndex = POCShader.FindKernel(name);
+	POCShader.SetBuffer(kernelIndex, "input", input);
+	POCShader.Dispatch(kernelIndex, 32, 32, 1);
+	return input;
+    }
+
+    public void computeSurface(float[] voxels, int width, int height, int depth, out Vector3[] vertices, out int[] indices) {
+      int kernelIndex = POCShader.FindKernel("MarchingCubes");
+      ComputeBuffer buffer = new ComputeBuffer(voxels.Length, sizeof(float));
+      ComputeBuffer vertexBuffer = new ComputeBuffer(voxels.Length, sizeof(float)*3);
+      ComputeBuffer indexBuffer = new ComputeBuffer(voxels.Length*3, sizeof(int));
+      buffer.SetData(voxels);
+
+      POCShader.SetBuffer(kernelIndex, "voxels", buffer);
+      POCShader.SetBuffer(kernelIndex, "vertices", vertexBuffer);
+      POCShader.SetBuffer(kernelIndex, "indices", indexBuffer);
+      POCShader.Dispatch(kernelIndex, width, height, depth);
+
+      vertices = new Vector3[voxels.Length];
+      float[] vertexOutput = new float[voxels.Length*3];
+      vertexBuffer.GetData(vertexOutput);
+      for(int i = 0; i < vertices.Length; i++) {
+	vertices[i] = new Vector3(vertexOutput[i*3], vertexOutput[i*3+1], vertexOutput[i*3+2]);
+      }
+
+      indices = new int[voxels.Length*3];
+      indexBuffer.GetData(indices);
+
+      buffer.Release();
+      vertexBuffer.Release();
+      indexBuffer.Release();
+    }
+}
---
+++ Assets/Scripts/Shader/Tests/POCTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class POCTest
+    {
+	POC poc = Component.FindObjectOfType<POC>();
+
+        [Test]
+        public void POCTestSimplePasses()
+        {
+	    float[] output = new float[1024];
+
+	    for(int i = 0; i < output.Length; i++) {
+		output[i] = (float)i;
+	    }
+
+	    ComputeBuffer buffer = new ComputeBuffer(1024, sizeof(float));
+	    buffer.SetData(output);
+	    poc.invokeShader("dataTransferTest", buffer);
+	    buffer.GetData(output);
+
+	    for(int i = 0; i < output.Length; i++) {
+	      Assert.AreEqual(true, output[i] == 1.0f);
+	    }
+
+	    buffer.Release();
+        }
+
+        [Test]
+        public void POCTestMarchingPasses()
+        {
+	    float[] voxels = new float[512];
+	    Vector3[] vertices;
+	    int[] indices;
+
+	    for(int i = 0; i < voxels.Length; i++) {
+		voxels[i] = (float)i;
+	    }
+
+	    poc.computeSurface(voxels, 8, 8, 8, out vertices, out indices);
+
+	    for(int i = 0; i < indices.Length; i++) {
+	      Assert.AreEqual(1, indices[i]);
+	    }
+        }
+
+    }
+}
