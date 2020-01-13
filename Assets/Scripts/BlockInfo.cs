d193345c39610a44da6099c8737a81b5ce4cb701
schmidt <henri.schmidt@adesso.de>
2020-01-13 17:04:22 +0100

    stuff

--- Assets/Scripts/BlockInfo.cs
+++ Assets/Scripts/BlockInfo.cs
@@ -9,27 +9,4 @@ public class BlockInfo : MonoBehaviour
     public void Start()
     {
     }
-
-    /*public void Update() {
-      Vector3[] norms = mesh.normals;
-      Vector3[] verts = mesh.vertices;
-
-      if (Block.Offset.x == 0 && Block.Offset.y == 0) {
-        for(int x = -Block.Overlap; x < Block.VoxelCount.x+1; x++) {
-      for(int y = -Block.Overlap; y < Block.VoxelCount.y+1; y++) {
-        for(int z = -Block.Overlap; z < Block.VoxelCount.z+1; z++) {
-          Debug.DrawLine(transform.TransformPoint(new Vector3(-Block.Overlap, y, z)), transform.TransformPoint(new Vector3(Block.VoxelCount.x, y, z)));
-          Debug.DrawLine(transform.TransformPoint(new Vector3(x, -Block.Overlap, z)), transform.TransformPoint(new Vector3(x, Block.VoxelCount.y, z)));
-          Debug.DrawLine(transform.TransformPoint(new Vector3(x, y, -Block.Overlap)), transform.TransformPoint(new Vector3(x, y, Block.VoxelCount.z)));
-        }
-      }
-        }
-        for(int i = 0; i < norms.Length; i++) {
-      Vector3 vert = transform.TransformPoint(verts[i]);
-      Vector3 normal = transform.TransformDirection(norms[i]);
-        Debug.DrawLine(vert, vert + 3.0f*normal, Color.yellow, 0.0f, true);
-        }
-      }
-    }
-  */
 }
--- Assets/Scripts/Nodes/NoiseTraversal.cs
+++ Assets/Scripts/Nodes/NoiseTraversal.cs
@@ -5,102 +5,103 @@ using System.Linq;
 
 public class NoiseTraversal : NodeCanvasTraversal
 {
-  // A list of Nodes from which calculation originates -> Call StartCalculation
-  public List<Node> workList;
+    // A list of Nodes from which calculation originates -> Call StartCalculation
+    public List<Node> workList;
 
-  public List<Node> cache = new List<Node>();
+    public List<Node> cache = new List<Node>();
 
-  public NoiseTraversal (NodeCanvas canvas) : base(canvas) {}
+    public NoiseTraversal(NodeCanvas canvas) : base(canvas) { }
 
-  /// <summary>
-  /// Recalculate from every node regarded as an input node
-  /// </summary>
-  public override void TraverseAll () 
-  {
-    PopulateCache();
-    if(cache.Any()) {
-      cache.ForEach(n => n.Calculate());
-      return;
-    }
-  }
-
-  /// <summary>
-  /// Recalculate from the specified node
-  /// </summary>
-  public override void OnChange (Node node) 
-  {
-    (nodeCanvas as NoiseCanvasType).StopComputation();
-    //repopulate cache on change
-    cache = new List<Node>();
-    PopulateCache();
-    (nodeCanvas as NoiseCanvasType).StartComputation(cache);
-  }
-
-  private void PopulateCache() {
-    workList = new List<Node> ();
-    for (int i = 0; i < nodeCanvas.nodes.Count; i++) 
+    /// <summary>
+    /// Recalculate from every node regarded as an input node
+    /// </summary>
+    public override void TraverseAll()
     {
-      Node node = nodeCanvas.nodes[i];
-      if (node.isInput ())
-      { // Add all Inputs
-	node.ClearCalculation ();
-	workList.Add (node);
-      }
+        PopulateCache();
+        if (cache.Any())
+        {
+            (nodeCanvas as NoiseCanvasType).StartComputation(cache);
+        }
     }
-    StartCalculation ();
-  }
 
-  private void StartCalculation () 
-  {
-    if (workList == null || workList.Count == 0)
-      return;
-
-    bool limitReached = false;
-    while (!limitReached)
-    { // Runs until the whole workList is calculated thoroughly or no further calculation is possible
-      limitReached = true;
-      for (int workCnt = 0; workCnt < workList.Count; workCnt++)
-      { // Iteratively check workList
-	if (ContinueCalculation (workList[workCnt]))
-	  limitReached = false;
-      }
-    }
-    if (workList.Count > 0)
+    /// <summary>
+    /// Recalculate from the specified node
+    /// </summary>
+    public override void OnChange(Node node)
     {
-      Debug.LogError("Did not complete calculation! " + workList.Count + " nodes block calculation from advancing!");
-      foreach (Node node in workList)
-	Debug.LogError("" + node.name + " blocks calculation!");
+        (nodeCanvas as NoiseCanvasType).StopComputation();
+        //repopulate cache on change
+        cache = new List<Node>();
+        PopulateCache();
+        (nodeCanvas as NoiseCanvasType).StartComputation(cache);
     }
-  }
 
-  private bool ContinueCalculation (Node node) 
-  {
-    if (node.calculated && !node.AllowRecursion)
-    { // Already calulated
-      workList.Remove (node);
-      return true;
+    private void PopulateCache()
+    {
+        workList = new List<Node>();
+        for (int i = 0; i < nodeCanvas.nodes.Count; i++)
+        {
+            Node node = nodeCanvas.nodes[i];
+            if (node.isInput())
+            { // Add all Inputs
+                node.ClearCalculation();
+                workList.Add(node);
+            }
+        }
+        StartCalculation();
     }
-    if (node.ancestorsCalculated ())
-    { // Calculation was successful
-      node.calculated = true;
-      cache.Add(node);
-      workList.Remove (node);
-      if (node.ContinueCalculation)
-      { // Continue with children
-	for (int i = 0; i < node.outputPorts.Count; i++)
-	{
-	  ConnectionPort outPort = node.outputPorts[i];
-	  for (int t = 0; t < outPort.connections.Count; t++)
-	    ContinueCalculation(outPort.connections[t].body);
-	}
-      }
-      return true;
+
+    private void StartCalculation()
+    {
+        if (workList == null || workList.Count == 0)
+            return;
+
+        bool limitReached = false;
+        while (!limitReached)
+        { // Runs until the whole workList is calculated thoroughly or no further calculation is possible
+            limitReached = true;
+            for (int workCnt = 0; workCnt < workList.Count; workCnt++)
+            { // Iteratively check workList
+                if (ContinueCalculation(workList[workCnt]))
+                    limitReached = false;
+            }
+        }
+        if (workList.Count > 0)
+        {
+            Debug.LogError("Did not complete calculation! " + workList.Count + " nodes block calculation from advancing!");
+            foreach (Node node in workList)
+                Debug.LogError("" + node.name + " blocks calculation!");
+        }
     }
-    else if (!workList.Contains (node)) 
-    { // Calculation failed, record to calculate later on
-      workList.Add (node);
+
+    private bool ContinueCalculation(Node node)
+    {
+        if (node.calculated && !node.AllowRecursion)
+        { // Already calulated
+            workList.Remove(node);
+            return true;
+        }
+        if (node.ancestorsCalculated())
+        { // Calculation was successful
+            node.calculated = true;
+            cache.Add(node);
+            workList.Remove(node);
+            if (node.ContinueCalculation)
+            { // Continue with children
+                for (int i = 0; i < node.outputPorts.Count; i++)
+                {
+                    ConnectionPort outPort = node.outputPorts[i];
+                    for (int t = 0; t < outPort.connections.Count; t++)
+                        ContinueCalculation(outPort.connections[t].body);
+                }
+            }
+            return true;
+        }
+        else if (!workList.Contains(node))
+        { // Calculation failed, record to calculate later on
+            workList.Add(node);
+        }
+        return false;
     }
-    return false;
-  }
 }
 
